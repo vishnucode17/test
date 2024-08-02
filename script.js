443e85b87131517a693230e7d80e339e20bd21a0
vishnucode17 <61362200+vishnucode17@users.noreply.github.com>
2024-08-02 16:58:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('July 17 2024, 00:00:00').getTime();
+    const countDate = new Date('October 17 2024, 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
